d6d32c85f7e5ba789063977430572721b3a4f34f
camillagianchini <gianchinicamilla@gmail.com>
2025-03-04 19:02:31 +0100

    HumanPlayer, fatto classi AWGameMode e AWGameInstance

---
+++ PA5316005/Source/PA5316005/Public/AWGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AWGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PA5316005_API UAWGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ PA5316005/Source/PA5316005/Public/AWGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AWGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PA5316005_API AAWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
